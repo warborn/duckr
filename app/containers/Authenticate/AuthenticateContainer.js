a32290e6f22adc9c3564e4c9864c5a9d332969f2
Ivan Munguia <xxthecreatorxx_live@hotmail.com>
2017-07-14 20:03:37 -0500

    Add redux thunks to handle the action dispatching

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -3,19 +3,11 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Authenticate } from 'components'
-import { auth } from 'helpers/auth'
 import * as userActionCreators from 'redux/modules/users'
 
 class AuthenticateContainer extends Component {
   handleAuth () {
-    this.props.fetchingUser()
-    auth().then((user) => {
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-    })
-      .catch((error) => {
-        this.props.fetchingUserFailure(error)
-      })
+    this.props.fetchAndHandleAuthedUser()
   }
 
   render () {
@@ -31,10 +23,7 @@ class AuthenticateContainer extends Component {
 AuthenticateContainer.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   error: PropTypes.string.isRequired,
-  authUser: PropTypes.func.isRequired,
-  fetchingUser: PropTypes.func.isRequired,
-  fetchingUserFailure: PropTypes.func.isRequired,
-  fetchingUserSuccess: PropTypes.func.isRequired,
+  fetchAndHandleAuthedUser: PropTypes.func.isRequired
 }
 
 function mapStateToProps (state) {
